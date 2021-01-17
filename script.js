97fd0cdedac97e074f9551c7ef2238f57ba3812c
rustamnew <1rustamnew1@gmail.com>
2021-01-17 22:05:08 +0300

    4

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-ширина < 1540
+let screenWidth = window.innerWidth
+
+let slideWidth = 600
+
+if (screenWidth < 768) {
+    slideWidth = 400
+}
+
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        slideWidth: slideWidth,
+        infiniteLoop: false
+    });
+  });
+
+$(document).ready(function(){
+    $('.slider-diplom').bxSlider({
+        slideWidth: slideWidth,
+        infiniteLoop: false
+    });
+});
