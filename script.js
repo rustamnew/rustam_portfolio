50612821fbfa36851659ac7a8273a0c14296a2d4
rustamnew <1rustamnew1@gmail.com>
2021-01-21 18:16:44 +0300

    6

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let slideWidth = 600
 if (screenWidth < 768) {
     slideWidth = 400
 }
+if (screenWidth <= 320 ) {
+    slideWidth = 300
+}
 
 $(document).ready(function(){
     $('.slider').bxSlider({
@@ -19,3 +22,14 @@ $(document).ready(function(){
         infiniteLoop: false
     });
 });
+let contacts = document.querySelector('#contacts')
+let contactsButton = document.querySelector('#contacts-button')
+let contactsClose = document.querySelector('#contacts-close')
+
+contactsButton.addEventListener('click', (e) => {
+    contacts.style.display = 'flex'
+})
+
+contactsClose.addEventListener('click', (e) => {
+    contacts.style.display = 'none'
+})
