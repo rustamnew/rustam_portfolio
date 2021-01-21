db9be0f8088455d61978b7e203286ef015b765d2
rustamnew <1rustamnew1@gmail.com>
2021-01-21 18:36:10 +0300

    6.1

--- script.js
+++ script.js
@@ -25,11 +25,21 @@ $(document).ready(function(){
 let contacts = document.querySelector('#contacts')
 let contactsButton = document.querySelector('#contacts-button')
 let contactsClose = document.querySelector('#contacts-close')
+let wrapper = document.querySelector('.wrapper')
 
 contactsButton.addEventListener('click', (e) => {
     contacts.style.display = 'flex'
+    wrapper.style.height = 0
 })
 
 contactsClose.addEventListener('click', (e) => {
     contacts.style.display = 'none'
+    wrapper.style.height = 'auto'
 })
+
+let viewports = document.createElement('div')
+viewports.style.position = 'absolute'
+viewports.style.right = 0
+viewports.style.top = 0
+viewports.innerHTML = screenWidth
+wrapper.append(viewports)
